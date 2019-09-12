f65e91a9314b3e6b786cb03b74d7a83484369b1d
Lucas Estienne <lucas.estienne@cira.ca>
2019-09-11 21:11:14 -0400

    exercise 1

---
+++ exercises/1/exercise1.go
@@ -0,0 +1,14 @@
+// CST8333 Exercise 1 - Lucas Estienne
+
+package main
+
+import (
+	"fmt"
+)
+
+// main function, this is the entrypoint
+func main() {
+	// output to stdout
+	fmt.Println("CheeseId,CheeseNameEn,CheeseNameFr,ManufacturerNameEn,ManufacturerNameFr,ManufacturerProvCode,ManufacturingTypeEn,ManufacturingTypeFr,WebSiteEn,WebSiteFr,FatContentPercent,MoisturePercent,ParticularitiesEn,ParticularitiesFr,FlavourEn,FlavourFr,CharacteristicsEn,CharacteristicsFr,RipeningEn,RipeningFr,Organic,CategoryTypeEn,CategoryTypeFr,MilkTypeEn,MilkTypeFr,MilkTreatmentTypeEn,MilkTreatmentTypeFr,RindTypeEn,RindTypeFr,LastUpdateDate")
+	fmt.Println("Lucas Estienne")
+}
